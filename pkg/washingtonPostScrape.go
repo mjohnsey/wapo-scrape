14275e7b0fbcf8183573151e4aad0a54c051af66
Michael Johnsey <michael.johnsey@ookla.com>
2019-01-03 22:58:16 -0600

    Switch to go (#4)
    * Add go scrape
    
    * Remove ruby implementation
    
    * Encapsulate scraper with an object
    
    * Wrap the scrape in a runner struct
    
    * Add module
    
    * Add cobra
    
    * Package scrape code outside of CLI
    
    * Add license

---
+++ pkg/washingtonPostScrape.go
@@ -0,0 +1,50 @@
+// Copyright © 2019 Michael Johnsey <mjohnsey@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+package pkg
+
+import (
+	"strconv"
+	"time"
+)
+
+// WashingtonPostScrape represents the result of scraping the Washington Post for headlines
+type WashingtonPostScrape struct {
+	Headlines  *[]*Headline `json:"headlines,omitempty"`
+	ScrapeTime string       `json:"scrapeTime"`
+}
+
+// SetTimeToNow sets the scrape's timestamp to now in unix time
+func (s *WashingtonPostScrape) SetTimeToNow() {
+	s.ScrapeTime = strconv.FormatInt(time.Now().Unix(), 10)
+}
+
+// ScrapeWashingtonPost is a runner for scraping the Washington Post
+func (s WashingtonPostScrape) ScrapeWashingtonPost() (*WashingtonPostScrape, error) {
+	scraper := WashingtonPostScraper{}.CreateNewWashingtonPostScraper()
+	headlines, err := scraper.ScrapeHeadlines()
+	if err != nil {
+		return nil, err
+	}
+	scrape := WashingtonPostScrape{
+		Headlines: headlines,
+	}
+	scrape.SetTimeToNow()
+	return &scrape, nil
+}
